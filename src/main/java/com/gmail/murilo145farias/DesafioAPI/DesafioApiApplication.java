bdb1b36e7d96d3496fae959714f626102f4192c5
Murilo Farias <murilo145farias@gmail.com>
2021-02-14 23:42:21 -0300

    Colocar configuraçoes iniciais no Projeto(SpringBoot e DriverDB)

---
+++ src/main/java/com/gmail/murilo145farias/DesafioAPI/DesafioApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.murilo145farias.DesafioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/murilo145farias/DesafioAPI/DesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.murilo145farias.DesafioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
